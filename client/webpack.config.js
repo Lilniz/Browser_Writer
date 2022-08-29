c2afcd2d9ca12f7d84870e587bdc49158353da5b
Lizzle <leefboettcher@gmail.com>
2022-08-29 00:13:53 -0500

    webpack finish

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const { GenerateSW } = require("workbox-webpack-plugin");
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,9 +18,28 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: "Browser-writer",
+        short_name: "BWs",
+        description: "Starter Browser Text App",
+        // background_color: "#555555",
+        start_url: "./",
+        publicPath: "./",
+        inject: true,
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            ios: true,
+          },
+        ],
+      }),
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'J.A.T.E'
+        filename: "index.html",
+        title: 'J.A.T.E',
+        inject: "body",
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
@@ -31,6 +50,12 @@ module.exports = () => {
     module: {
       rules: [
         {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
